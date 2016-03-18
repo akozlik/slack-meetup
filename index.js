2877fe36cb5821fdd298c0a8df8d2f0ba7296a52
akozlik <akozlik@gmail.com>
2016-03-18 00:53:12 -0400

    Continued building out Meetup API

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+var meetup = require('./meetup-api.js');
+var slack = require('./slack.js');
+
 var express = require('express');
 
 var app = express();
@@ -15,7 +18,18 @@ app.get('/meetup', function(req, res) {
 
 app.get('/', function(req, res) {
     res.render('index');
+    meetup.events("iOS", receivedEvents);
 });
+
+function receivedEvents(body) {
+    console.log("========= GOT BODY =========");
+    console.log(body.results);
+
+    for (var i = 0; i < body.results.length; i++) {
+        console.log("===========");
+        console.log(body.results[i].group.name);
+    }
+}
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running on port " + app.get('port'));
