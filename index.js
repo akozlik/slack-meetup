c32272e2ed330def527193d3008b2f34b8606ec2
akozlik <akozlik@gmail.com>
2016-03-18 01:13:38 -0400

    Fixed port

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function receivedEvents(body) {
     }
 }
 
-app.listen(app.get('port') || 5000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log("Node app is running on port " + app.get('port'));
 });
