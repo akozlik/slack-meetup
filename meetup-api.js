2397ed39f38cc5d9a229b52d6ee0fe96eab59ac0
akozlik <akozlik@gmail.com>
2016-03-29 00:54:04 -0400

    updating meetup api

--- meetup-api.js
+++ meetup-api.js
@@ -6,8 +6,8 @@ module.exports = {
     events : function(query, callback) {
 
         var apiKey = process.env.MEETUP_API_KEY;
-        console.log(apiKey);
-        client.get('open_events?&sign=true&photo-host=public&zip=32771&text=iOS&page=20&key='+apiKey,
+
+        client.get('open_events?&sign=true&photo-host=public&zip=32801&text=' + query + '&page=20&key='+apiKey,
             function(err, res, body) {
                 callback(body);
             }
