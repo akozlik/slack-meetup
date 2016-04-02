423abab2aa6e1796f1fa8cc1797b639736e28ec4
akozlik <akozlik@gmail.com>
2016-04-01 23:24:42 -0400

    Commenting

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 require('dotenv').config();
-var bodyParser = require('body-parser');
 
+var bodyParser = require('body-parser');
 var meetup = require('./meetup-api.js');
 var slack = require('./slack.js');
 var striptags = require('striptags');
 var dateFormat = require('dateformat');
 var moment = require("moment");
-
 var express = require('express');
-
-var app = express();
 var hbs = require('hbs');
+
+// Instantiate a new express app
+var app = express();
+
 var response_url = "";
 
+// Search topics for meetup
 var topicsArray = ["iOS","android","react","php","javascript","iot","dotnet","ruby","angular","aws","bigdata","clojure","css","drupal","wordpress","programming","java","node","swift","objective-c","sql"];
 
+// Configure the express app
 app.set('port', 3000);
 app.set('view engine', 'html');
 
+// Specify handlebars for templating
 app.engine('html', hbs.__express);
 
+// Mount bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 
-/* Routes */
-
+// Set up the GET route
 app.get('/meetup', function(req, res) {
     res.send("Meetup");
 });
 
+// Set up the POST route
 app.post('/meetup', function(req, res, next) {
 
     response_url = req.body.response_url;
